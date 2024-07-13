a8308e70814a7b385ba54a6b82d9d2b47543ab38
Sahil Hussain <mdsahil37621@gmail.com>
2024-07-13 09:00:06 +0530

    sidebar complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
-import './App.css';
+import "./App.css";
+import gptLogo from "./assets/chatgpt.svg";
+import addBtn from "./assets/add-30.png";
+import msgIcon from "./assets/message.svg";
+import home from "./assets/home.svg";
+import saved from "./assets/bookmark.svg";
+import rocket from "./assets/rocket.svg";
 
 function App() {
   return (
     <div className="App">
-      React App
+      <div className="sidebar">
+        <div className="upperSide">
+          <div className="upperSideTop">
+            <div className="nameAndLogo">
+              <img src={gptLogo} alt="Logo" className="logo" />
+              <span className="brand">ChatGPT</span>
+            </div>
+            <button className="midBtn">
+              <img src={addBtn} alt="new chat" className="addBtn" />
+              New Chat
+            </button>
+            <div className="upperSideBottom">
+              <button className="query">
+                <img src={msgIcon} alt="Query" />
+                What is Programming ?
+              </button>
+              <button className="query">
+                <img src={msgIcon} alt="Query" />
+                How to use an API ?
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="lowerSide">
+          <div className="listItems">
+            <img src={home} alt="" className="listItemImg" />
+            Home
+          </div>
+          <div className="listItems">
+            <img src={saved} alt="" className="listItemImg" />
+            Saved
+          </div>
+          <div className="listItems">
+            <img src={rocket} alt="" className="listItemImg" />
+            Upgrade to Pro
+          </div>
+        </div>
+      </div>
+      <div className="main">main</div>
     </div>
   );
 }
